e10851ac1f94d80c5a5ee5018afefcc6fa07cb29
poignant-semicolon-2878 <jndossantos00@gmail.com>
2020-10-16 18:24:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,6 @@
 def display_board
-  puts "A Tic Tac Toe Board"
-  puts "A Tic Tac Toe Board"
-  puts "A Tic Tac Toe Board"
-end
+  puts "  | | "
+  puts "-----------"
+  puts "  | | "
+  puts "-----------"
+  puts "  | | "
